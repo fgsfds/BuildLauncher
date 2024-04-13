b7de519a79f3474b73267a7c870888400b948bca
fgsfds <4870330+fgsfds@users.noreply.github.com>
2024-04-13 15:15:29 +0500

    fury fix

--- src/Ports/Ports/EDuke32/Fury.cs
+++ src/Ports/Ports/EDuke32/Fury.cs
@@ -2,7 +2,10 @@
 using Common.Enums;
 using Common.Helpers;
 using Common.Interfaces;
+using Games.Games;
+using Mods.Mods;
 using Ports.Providers;
+using System.Text;
 
 namespace Ports.Ports.EDuke32
 {
@@ -45,6 +48,71 @@ namespace Ports.Ports.EDuke32
         protected override void BeforeStart(IGame game, IAddon campaign)
         {
             FixGrpInConfig();
+        }
+
+
+        /// <inheritdoc/>
+        protected override void GetStartCampaignArgs(StringBuilder sb, IGame game, IAddon mod)
+        {
+            if (mod.MainDef is not null)
+            {
+                sb.Append($@" {MainDefParam}""{mod.MainDef}""");
+            }
+            //no need to override main def
+
+            if (mod.AdditionalDefs is not null)
+            {
+                foreach (var def in mod.AdditionalDefs)
+                {
+                    sb.Append($@" {AddDefParam}""{def}""");
+                }
+            }
+
+
+            if (game is FuryGame fGame && mod is FuryCampaign fCamp)
+            {
+                GetFuryArgs(sb, fGame, fCamp);
+            }
+            else
+            {
+                ThrowHelper.NotImplementedException($"Mod type {mod.Type} for game {game} is not supported");
+            }
+        }
+
+        private void GetFuryArgs(StringBuilder sb, FuryGame game, FuryCampaign camp)
+        {
+            if (camp.FileName is null)
+            {
+                return;
+            }
+
+
+            if (camp.Type is AddonTypeEnum.TC)
+            {
+                sb.Append($@" {AddFileParam}""{Path.Combine(game.CampaignsFolderPath, camp.FileName)}""");
+
+                if (camp.MainCon is not null)
+                {
+                    sb.Append($@" {MainConParam}""{camp.MainCon}""");
+                }
+
+                if (camp.AdditionalCons?.Count > 0)
+                {
+                    foreach (var con in camp.AdditionalCons)
+                    {
+                        sb.Append($@" {AddConParam}""{con}""");
+                    }
+                }
+            }
+            else if (camp.Type is AddonTypeEnum.Map)
+            {
+                GetMapArgs(sb, game, camp);
+            }
+            else
+            {
+                ThrowHelper.NotImplementedException($"Mod type {camp.Type} is not supported");
+                return;
+            }
         }
     }
 }
