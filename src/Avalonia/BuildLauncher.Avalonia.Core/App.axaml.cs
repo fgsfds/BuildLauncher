0d77f4a5425ba414d1b2b395c683667c0d807bd1
fgsfds <4870330+fgsfds@users.noreply.github.com>
2024-04-21 19:28:55 +0500

    moved tests

--- src/Avalonia/BuildLauncher.Avalonia.Core/App.axaml.cs
+++ src/Avalonia/BuildLauncher.Avalonia.Core/App.axaml.cs
@@ -25,26 +25,13 @@ public sealed partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var container = BindingsManager.Instance;
+        var str = string.Empty;
 
-        CommonBindings.Load(container);
-        ViewModelsBindings.Load(container);
-        Games.DI.ProvidersBindings.Load(container);
-        Ports.DI.ProvidersBindings.Load(container);
-        Mods.DI.ProvidersBindings.Load(container);
-        Updater.DI.ProvidersBindings.Load(container);
 
-        var theme = BindingsManager.Provider.GetRequiredService<ConfigProvider>().Config.Theme;
 
-        var themeEnum = theme switch
-        {
-            ThemeEnum.System => ThemeVariant.Default,
-            ThemeEnum.Light => ThemeVariant.Light,
-            ThemeEnum.Dark => ThemeVariant.Dark,
-            _ => ThrowHelper.ArgumentOutOfRangeException<ThemeVariant>(theme.ToString())
-        };
 
-        RequestedThemeVariant = themeEnum;
+        LoadBindings();
+        SetTheme();
 
         // Line below is needed to remove Avalonia data validation.
         // Without this line you will get duplicate validations from both Avalonia and CT
@@ -65,5 +52,32 @@ public sealed partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void LoadBindings()
+    {
+        var container = BindingsManager.Instance;
+
+        CommonBindings.Load(container);
+        ViewModelsBindings.Load(container);
+        Games.DI.ProvidersBindings.Load(container);
+        Ports.DI.ProvidersBindings.Load(container);
+        Mods.DI.ProvidersBindings.Load(container);
+        Updater.DI.ProvidersBindings.Load(container);
+    }
+
+    private void SetTheme()
+    {
+        var theme = BindingsManager.Provider.GetRequiredService<ConfigProvider>().Config.Theme;
+
+        var themeEnum = theme switch
+        {
+            ThemeEnum.System => ThemeVariant.Default,
+            ThemeEnum.Light => ThemeVariant.Light,
+            ThemeEnum.Dark => ThemeVariant.Dark,
+            _ => ThrowHelper.ArgumentOutOfRangeException<ThemeVariant>(theme.ToString())
+        };
+
+        RequestedThemeVariant = themeEnum;
     }
 }
