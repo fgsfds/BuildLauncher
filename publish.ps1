0c99c3262bb07731394245e2273db6579605dba1
fgsfds <4870330+fgsfds@users.noreply.github.com>
2024-05-24 13:58:04 +0500

    added publish scripts

---
+++ publish.ps1
@@ -0,0 +1,7 @@
+dotnet publish ".\src\Avalonia\Desktop\Avalonia.Desktop.csproj" -p:PublishProfile=Windows
+
+$version = (Get-Item .\publish\BuildLauncher.exe).VersionInfo.FileVersion
+$version = $version.Substring(0,$version.Length-2)
+$version = $version.Replace('.','')
+
+Compress-Archive -Path ".\publish\BuildLauncher.exe" -DestinationPath ".\publish\buildlauncher_${version}_win-x64.zip"
