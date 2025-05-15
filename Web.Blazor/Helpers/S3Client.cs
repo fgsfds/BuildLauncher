368939905d261c18e9150810871d36b0baf5db1b
fgsfds <4870330+fgsfds@users.noreply.github.com>
2025-05-15 12:59:30 +0500

    added loose files test

--- Web.Blazor/Helpers/S3Client.cs
+++ Web.Blazor/Helpers/S3Client.cs
@@ -1,28 +1,7 @@
-﻿using Amazon.Runtime;
-using Amazon.S3;
-using Amazon.S3.Model;
-
-namespace Web.Blazor.Helpers;
+﻿namespace Web.Blazor.Helpers;
 
 public sealed class S3Client : IDisposable
 {
-    private readonly string _key = Environment.GetEnvironmentVariable("S3Key")!;
-    private readonly string _secretKey = Environment.GetEnvironmentVariable("S3SKey")!;
-
-    private readonly AmazonS3Client _s3client;
-
-    public S3Client()
-    {
-        AmazonS3Config config = new()
-        {
-            ServiceURL = "https://s3.timeweb.cloud",
-            UseHttp = false
-        };
-
-        AWSCredentials creds = new BasicAWSCredentials(_key, _secretKey);
-        _s3client = new(creds, config);
-    }
-
     /// <summary>
     /// Get signed URL for file uploading
     /// </summary>
@@ -30,18 +9,11 @@ public sealed class S3Client : IDisposable
     /// <returns>Signed URL</returns>
     public string GetSignedUrl(string file)
     {
-        GetPreSignedUrlRequest request = new()
-        {
-            //BucketName = Consts.Bucket,
-            Key = file,
-            Expires = DateTime.Now.AddHours(1),
-            Verb = HttpVerb.PUT
-        };
-
-        var url = _s3client.GetPreSignedURL(request);
-
-        return url;
+        return string.Empty;
     }
 
-    public void Dispose() => _s3client?.Dispose();
+    public void Dispose()
+    {
+
+    }
 }
