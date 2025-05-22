0c3f5eb4cecf9addf12dfe3b8908e95219802721
fgsfds <4870330+fgsfds@users.noreply.github.com>
2025-05-22 23:19:39 +0500

    added missing dispose

--- src/Avalonia.Desktop/MainWindow.axaml.cs
+++ src/Avalonia.Desktop/MainWindow.axaml.cs
@@ -7,17 +7,26 @@ namespace Avalonia.Desktop;
 
 public sealed partial class MainWindow : Window
 {
+    private readonly Stream? _overlayStream;
+
     public MainWindow()
     {
 
 #if DEBUG
         this.AttachDevTools();
 #endif
+
         RenderOptions.SetBitmapInterpolationMode(this, BitmapInterpolationMode.HighQuality);
 
         InitializeComponent();
 
         var uri = new Uri("avares://BuildLauncher/Assets/overlay.png");
-        Resources["HighlightOverlayBitmap"] = new Bitmap(AssetLoader.Open(uri));
+        _overlayStream = AssetLoader.Open(uri);
+        Resources["HighlightOverlayBitmap"] = new Bitmap(_overlayStream);
+    }
+
+    private void OnWindowClosing(object? sender, Avalonia.Controls.WindowClosingEventArgs e)
+    {
+        _overlayStream?.Dispose();
     }
 }
