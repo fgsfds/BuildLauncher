a5b9906f5fed3a8c59c1f1cf03d877ca72a54c88
fgsfds <4870330+fgsfds@users.noreply.github.com>
2026-01-22 19:59:24 +0500

    added app releases test

---
+++ src/Tests/AppReleasesTest.cs
@@ -0,0 +1,36 @@
+﻿using Common.All.Enums;
+using Common.All.Helpers;
+using Common.All.Providers;
+using Moq;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace Tests;
+
+public sealed class AppReleasesTest
+{
+    [Fact]
+    public async Task DeserializeAddonJsonAsync()
+    {
+        using HttpClient httpClient = new();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "BuildLauncher");
+        var logger = new Mock<ILogger>().Object;
+        Mock<IHttpClientFactory> httpFactory = new();
+        httpFactory.Setup(x => x.CreateClient(HttpClientEnum.GitHub.GetDescription())).Returns(httpClient);
+
+        RepoAppReleasesProvider releasesProvider = new(logger, httpFactory.Object);
+
+        var lastestRelease = await releasesProvider.GetLatestReleaseAsync(false);
+
+        Assert.NotNull(lastestRelease);
+        Assert.NotEmpty(lastestRelease);
+    }
+}
+
+
+
+
+
+
+
+
+
